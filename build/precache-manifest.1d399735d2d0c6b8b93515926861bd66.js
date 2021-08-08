04e336b8d228f637601aba9f2f824dca7d5a0bd1
Yasniel PC <yasnielfajardoegues1408@gmail.com>
2021-08-08 03:35:25 -0400

    add build

---
+++ build/precache-manifest.1d399735d2d0c6b8b93515926861bd66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7b2fd78094907ee382cfc63b1e52d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e0ebc2e04eeac9ba308",
+    "url": "/static/css/main.909567f9.chunk.css"
+  },
+  {
+    "revision": "9fe6f3ca791c58a8d0aa",
+    "url": "/static/js/2.4a1c0152.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/static/js/2.4a1c0152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e0ebc2e04eeac9ba308",
+    "url": "/static/js/main.e11811e7.chunk.js"
+  },
+  {
+    "revision": "b392ba862a5bbfe037c8",
+    "url": "/static/js/runtime-main.f24b64df.js"
+  }
+]);
